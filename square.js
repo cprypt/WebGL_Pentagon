3862360cf4da70f26af4a75b834bc89856326562
Suyoung Seo <32202145@dankook.ac.kr>
2023-09-21 13:07:53 +0900

    square implement

---
+++ square.js
@@ -0,0 +1,46 @@
+// 3. 계산 및 데이터 지정
+var gl;
+var points;
+
+window.onload = function init() {
+    var canvas = document.getElementById("gl-canvas");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) { alert("WebGL isn't available"); }
+
+    // Four Vertices
+    var vertices = [
+        vec2(-0.1, -0.2),
+        vec2(-0.2, 0),
+        vec2(0, 0.2),
+        vec2(0.2, 0),
+        vec2(0.1, -0.2)
+    ];
+
+    // Configure WebGL
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    // Load shaders and initialize attribute buffers
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // 4. GPU에 데이터 전송
+    // Load the data into the GPU
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferId);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    // Associate out shader variables with our data buffer
+    var vPosition = gl.getAttribLocation(program, "vPosition");
+    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vPosition);
+
+    // 5. 데이터 렌더링
+    render();
+};
+
+function render() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 5);
+}
